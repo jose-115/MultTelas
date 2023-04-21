a4e6527145c4b69cc72c2cc2a6352814ffb0b44c
jose.115 <joseveiga1234@gmail.com>
2023-04-20 21:29:15 -0400

    Adição do link para o site da UFMS

--- app/src/main/java/com/example/multiplastelas/Sobre.java
+++ app/src/main/java/com/example/multiplastelas/Sobre.java
@@ -1,6 +1,7 @@
 package com.example.multiplastelas;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,7 +10,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class Sobre extends AppCompatActivity {
 
-    Button voltar;
+    Button voltar, ufms;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +18,16 @@ public class Sobre extends AppCompatActivity {
         setContentView(R.layout.sobre);
 
         voltar = findViewById(R.id.btVoltar);
+        ufms = findViewById(R.id.btUfms);
 
+        ufms.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri = Uri.parse("https://www.ufms.br/");
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+        });
 
         voltar.setOnClickListener(new View.OnClickListener() {
             @Override
